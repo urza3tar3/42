abda8f7e836828b3a8ba070ad17479425c967b6d
Tala <talaalqam45@gmail.com>
2025-02-06 08:52:18 +0300

    added ex04 fixed while loop and norminette

--- ex04/ft_print_numbers.c
+++ ex04/ft_print_numbers.c
@@ -1,13 +1,28 @@
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: talqam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 14:28:11 by talqam            #+#    #+#             */
+/*   Updated: 2025/02/05 14:48:49 by talqam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <stdio.h>
 #include <unistd.h>
 
-void ft_print_numbers(void){
-	char num;
-	for( num = 48 ; num <= 57 ; num++){
+void	ft_print_numbers(void)
+{
+	char	num;
+
+	num = 48;
+	while (num < 57)
+	{
 		write(1, &num, 1);
-			}
+		num++;
+	}
 }
-int main (){
+
+int	main(void)
+{
 	ft_print_numbers();
-	return 0;
 }
